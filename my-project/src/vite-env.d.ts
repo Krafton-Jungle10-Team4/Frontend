aa61e9b6bd88cbc555a9eb5cc893f09b19b1e3a6
hyeondxo <eun041212@gmail.com>
2025-10-31 14:42:21 +0900

    feat: React 프로젝트 초기 세팅 완료

---
+++ my-project/src/vite-env.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="vite/client" />
+
+// SVG 파일을 모듈로 import 할 수 있도록 타입 선언
+declare module '*.svg' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.png' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.jpg' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.jpeg' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.gif' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.webp' {
+    const content: string;
+    export default content;
+  }
