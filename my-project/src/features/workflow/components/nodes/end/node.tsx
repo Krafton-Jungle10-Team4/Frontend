3d52038993e98bbe3cf9e62305a7bfdf7a6df5c8
OneVth <onebrotravel@gmail.com>
2025-11-09 22:51:27 +0900

    refactor: Start/End 노드 카드 간소화 및 desc 필드 제거
    - BaseNode: desc (설명) 필드 표시 제거
    - Start Node: 아이콘과 제목만 표시
    - End Node: 아이콘과 제목만 표시

--- my-project/src/features/workflow/components/nodes/end/node.tsx
+++ my-project/src/features/workflow/components/nodes/end/node.tsx
@@ -3,16 +3,11 @@ import { memo } from 'react';
 
 /**
  * End 노드
- * 워크플로우 종료점 (최종 답변 출력)
+ * 워크플로우 종료점
+ * - 아이콘과 제목만 표시 (BaseNode에서 제공)
  */
 const EndNode = ({ data: _data }: NodeProps<EndNodeType>) => {
-  return (
-    <div className="px-3 py-1">
-      <div className="system-xs-regular text-text-tertiary text-center">
-        최종 답변 출력
-      </div>
-    </div>
-  );
+  return <></>;
 };
 
 export default memo(EndNode);
